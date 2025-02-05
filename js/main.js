967b15cd0eef5e405e6af22896be9597b7cf4b49
srikk <122169588+py-code314@users.noreply.github.com>
2025-02-04 21:33:09 -0500

    style: Add error-state styles

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
 // console.log('it works');
+const form = document.querySelector('.card__form');
+const email = document.querySelector('.card__input');
+const emailError = document.querySelector('.card__error');
+const dialog = document.querySelector('.card__success');
 
+form.addEventListener('submit', (event) => {
+  // const invalidInput = document.querySelector(':invalid');
+  if (!email.validity.valid) {
+    event.preventDefault();
+    showError()
+    email.focus()
+  } 
+  
+})
+
+function showError() {
+  if (email.validity.valueMissing) {
+    emailError.textContent = "Enter your email address"
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = "Valid email required"
+  }
+  emailError.className = "error-msg"
+  email.className = "card__input active"
+}
 
 
